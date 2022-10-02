b0d96b6457826d7a716010488581341678a7172f
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-10-02 19:49:21 +0300

    [Enh](Add-Bookmark): Simplified user interface.

--- Modules/Navigation/Bookmarks.psm1
+++ Modules/Navigation/Bookmarks.psm1
@@ -1,6 +1,24 @@
 # Create symbolic links in .bookmarks.
-function Add-Bookmark ($target, $bookmarkName) {
-    New-Item -ItemType SymbolicLink -Path ${Env:BookmarksPath}\${bookmarkName} -Value $target
+function Add-Bookmark() {
+    param(
+        [string]$target,
+        [string]$bookmarkName
+    )
+
+    if ($bookmarkName -eq "") {
+        $bookmarkName = Split-Path $target -leaf # Get filename.
+    }
+
+    $bookmarkPath = "${Env:BookmarksPath}\${bookmarkName}"
+
+    # Convert relative path to absolute.
+    $targetPath = $target
+
+    if (!(Split-Path -Path $target -IsAbsolute)) {
+        $targetPath = "${pwd}\${target}"
+    }
+
+    New-Item -ItemType SymbolicLink -Path ${bookmarkPath} -Value $targetPath
 }
 
 Export-ModuleMember -Function Add-Bookmark
