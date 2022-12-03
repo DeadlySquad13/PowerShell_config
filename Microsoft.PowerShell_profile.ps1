04cda4e815800267bf83214a1f8222136dbb54c1
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-03 11:19:47 +0300

    [Enh](Kbd): Added KbdBuff searching prototype implemented with fzf and ripgrep.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -63,6 +63,31 @@ function Get-DirectoryName {
 #  cat 
 #}
 
+# KbdBuff Seraching.
+function Invoke-FuzzyOpenInBrowser {
+    param(
+        [Parameter(Mandatory)]
+        $options
+        # [switch]$browser
+    )
+
+    $searchResult = Invoke-PsFzfRipgrep $options -NoEditor
+
+    if (-not $searchResult) {
+        return
+    }
+
+    $browserExecutable = "Chrome"
+    if ($(Get-DirectoryName $searchResult) -eq "Opera") {
+        $browserExecutable = "Opera"
+    }
+
+    return & $browserExecutable $searchResult
+}
+
+# function Invoke-FuzzyOpenInOpera($options) {
+#     "C:\Users\Александр\AppData\Local\Programs\Opera\launcher.exe" $(Invoke-PsFzfRipgrep $options -NoEditor)
+# }
 
 # Utilities.
 Import-Module EnvManagement
