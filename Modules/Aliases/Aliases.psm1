fd2d698014631ec1ddbd7ba20b58167b46ddb7ec
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-23 13:35:59 +0300

    [Enh](Aliases): Added for yazi.

--- Modules/Aliases/Aliases.psm1
+++ Modules/Aliases/Aliases.psm1
@@ -2,9 +2,17 @@
 Set-Alias ll dir
 Set-Alias b buku
 
+# Wanted something similar to 'r' for ranger in Linux.
+# At the same time can't just assign it to 'r' because
+# it's already used in Powershell and these two programs
+# behave differently.
+Set-Alias yr yazi
+
 # Nvim.
 Set-Alias vi nvim
 Set-Alias vim nvim
+# - Just for the sake of uniformity between Windows and Unix.
+Set-Alias vi-dev nvim
 
 Set-Alias lg lazygit
 
